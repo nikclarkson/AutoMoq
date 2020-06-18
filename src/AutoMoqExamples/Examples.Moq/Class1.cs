675a1a350787223a7fa3c8b1cd50742503a8f01a
armadilloNik <nik@tumpedover.com>
2020-06-17 23:31:26 -0500

    moq test project with deps

---
+++ src/AutoMoqExamples/Examples.Moq/Class1.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Examples.Moq
+{
+    public class Class1
+    {
+        [Fact]
+        public void yo()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
